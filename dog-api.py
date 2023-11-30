a26ef18f5c4f427c37f7bb94b360914565fbf868
samanthajarrah <samantha.jarrah2@gmail.com>
2023-11-29 21:04:28 -0800

    added docstring and error return statement

--- dog-api.py
+++ dog-api.py
@@ -4,13 +4,13 @@ import requests
 app = Flask(__name__)
     
 @app.route('/fact')
-def test():
+def get_fact():
+    """Makes request to dogapi.dog to get a dog fact"""
+
     response = requests.get("https://dogapi.dog/api/v2/facts")
     if response.status_code == 200:
         return response.json()
-    else:
-        print("Error making request!")
-     
+    return "Error making request!"
     
 if __name__ == '__main__':
     app.run(debug = True)
